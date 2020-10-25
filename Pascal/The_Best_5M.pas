d0bbb8a55a14c3d514ab21911824385b54b0c2b5
Rubens Zimbres <rubenszmm@gmail.com>
2020-10-25 12:20:20 -0300

    Update The_Best_5M.pas

--- Pascal/The_Best_5M.pas
+++ Pascal/The_Best_5M.pas
@@ -28,7 +28,7 @@ begin
   
   // BBAS3F
 
-  se (isBought = false) e ((ZeroLAG_MACD-ZeroLAG_MACD_Sinal) > 0) e (MACD(35,14,9)|1|>0) então
+  se (isBought = false) e ((ZeroLAG_MACD-ZeroLAG_MACD_Sinal) > 0) e (MACD(35,14,9)|1|>0.05) então
     
     inicio
       ClosePosition;  
