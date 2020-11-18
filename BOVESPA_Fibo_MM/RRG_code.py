b2a4531f5ab5bfc55459a6033bc9b51b5dc2827d
Rubens Zimbres <rubenszmm@gmail.com>
2020-11-18 16:07:52 -0300

    Update RRG_code.py

--- BOVESPA_Fibo_MM/RRG_code.py
+++ BOVESPA_Fibo_MM/RRG_code.py
@@ -11,10 +11,11 @@ from pandas_datareader import data
 import numpy as np
 from datetime import timedelta
 #pip install pandas-datareader
-
+import time
+start=time.time()
 hoje = datetime.datetime.today()
 #hoje=now.strftime("%Y,%m,%d")
-inicio=(datetime.datetime.today() - timedelta(days=144))#.strftime("%Y,%m,%d")
+inicio=(datetime.datetime.today() - timedelta(days=320))#.strftime("%Y,%m,%d")
       
 def predict(acao):
 
@@ -30,7 +31,7 @@ def predict(acao):
         goog_df = data.DataReader(stock, source, start, end)
         
         dataset = goog_df['Adj Close']
-        print(len(dataset))
+
         goog_df['Adj Close'].plot(kind='line', grid=True, title='{} Adjusted Closes, IPO through 2016'.format(stock))
         
         ## FIBO DINAMICO
@@ -43,21 +44,22 @@ def predict(acao):
         segundo_fibo=min_periodo+0.618*diff
         
         
-        media_verde=np.mean(dataset[-5:])
-        media_vermelha=np.mean(dataset[-7:])
+        media_verde=np.mean(dataset[-15:])
+        media_vermelha=np.mean(dataset[-21:])
         
         tendencia=media_verde-media_vermelha
-        print(tendencia)
-        print(acao)
+        print(acao,tendencia)
         fibo_superior=dataset[-1]-segundo_fibo
+        limiar_superior=(max_periodo-segundo_fibo)
+        limiar_inferior=(primeiro_fibo-min_periodo)
         
         
         fibo_inferior=dataset[-1]-primeiro_fibo
         
-        if tendencia>0 and fibo_superior>0 and fibo_superior<0.04*dataset[-1] and dataset[-1]>dataset[-3] and dataset[-1]>segundo_fibo:
+        if tendencia>0 and fibo_inferior>0 and  dataset[-1]>dataset[-5] and dataset[-1]>primeiro_fibo:
             output='buy'
             print('buy')
-        elif tendencia<0 and fibo_inferior<0 and fibo_inferior<-0.04*dataset[-1] and dataset[-1]<dataset[-3] and dataset[-1]<primeiro_fibo:
+        elif tendencia<0 and fibo_superior<0 and dataset[-1]<dataset[-5] and dataset[-1]<segundo_fibo:
             output='short'
             print('short')
         else:
@@ -88,5 +90,23 @@ for i in range(0,df.shape[0]):
     df['Potencial'].iloc[i]=predict(df.iloc[i,0])[2]
     df['MM_diff'].iloc[i]=predict(df.iloc[i,0])[3]
 
+df['Mood']=np.zeros(df.shape[0])
+
+for i in range(0,df.shape[0]):
+    if df['Previsao'][i]=='buy':
+        df['Mood'][i]=1
+    elif df['Previsao'][i]=='short':
+        df['Mood'][i]=-1
+    if df['Previsao'][i]=='wait':
+        df['Mood'][i]=0
+    else:
+        df['Mood'][i]=0
+
+df['Mood'].sum()
+
 df[df.Previsao=='buy']
 df.to_csv('recomendacoes_{}.csv'.format(hoje),sep=',',columns=df.columns,index=False)
+
+df.to_csv('recomendacoes_PowerBI.csv',sep=',',columns=df.columns,index=False)
+
+end=time.time()
