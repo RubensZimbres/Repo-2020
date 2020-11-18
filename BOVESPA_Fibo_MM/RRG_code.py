876398f0f346928da916f755941529a6a641338a
Rubens Zimbres <rubenszmm@gmail.com>
2020-11-18 10:58:41 -0300

    Update RRG_code.py

--- BOVESPA_Fibo_MM/RRG_code.py
+++ BOVESPA_Fibo_MM/RRG_code.py
@@ -14,7 +14,7 @@ from datetime import timedelta
 
 hoje = datetime.datetime.today()
 #hoje=now.strftime("%Y,%m,%d")
-#inicio=(datetime.datetime.today() - timedelta(days=200))#.strftime("%Y,%m,%d")
+inicio=(datetime.datetime.today() - timedelta(days=144))#.strftime("%Y,%m,%d")
       
 def predict(acao):
 
@@ -23,7 +23,7 @@ def predict(acao):
         source = 'yahoo'
         
         # Set date range (Google went public August 19, 2004)
-        start =datetime.datetime(2020, 3, 1)
+        start =inicio#datetime.datetime(2020, 3, 1)
         end = hoje
         
         # Collect Google stock data
@@ -43,8 +43,8 @@ def predict(acao):
         segundo_fibo=min_periodo+0.618*diff
         
         
-        media_verde=np.mean(dataset[-7:])
-        media_vermelha=np.mean(dataset[-12:])
+        media_verde=np.mean(dataset[-5:])
+        media_vermelha=np.mean(dataset[-7:])
         
         tendencia=media_verde-media_vermelha
         print(tendencia)
@@ -54,10 +54,10 @@ def predict(acao):
         
         fibo_inferior=dataset[-1]-primeiro_fibo
         
-        if tendencia>0 and fibo_superior>0 and fibo_superior<0.04*dataset[-1]:
+        if tendencia>0 and fibo_superior>0 and fibo_superior<0.04*dataset[-1] and dataset[-1]>dataset[-3] and dataset[-1]>segundo_fibo:
             output='buy'
             print('buy')
-        elif tendencia<0 and fibo_inferior<0 and fibo_inferior<-0.04*dataset[-1]:
+        elif tendencia<0 and fibo_inferior<0 and fibo_inferior<-0.04*dataset[-1] and dataset[-1]<dataset[-3] and dataset[-1]<primeiro_fibo:
             output='short'
             print('short')
         else:
@@ -86,7 +86,7 @@ for i in range(0,df.shape[0]):
     df['Previsao'].iloc[i]=predict(df.iloc[i,0])[0]
     df['Close'].iloc[i]=predict(df.iloc[i,0])[1]
     df['Potencial'].iloc[i]=predict(df.iloc[i,0])[2]
-    df['MM_diff'].iloc[i]=predict(df.iloc[i,0])[2]
+    df['MM_diff'].iloc[i]=predict(df.iloc[i,0])[3]
 
 df[df.Previsao=='buy']
 df.to_csv('recomendacoes_{}.csv'.format(hoje),sep=',',columns=df.columns,index=False)
