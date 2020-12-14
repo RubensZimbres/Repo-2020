c6bd5f547fb07b886bbdf343df2715a0591d18f3
Rubens Zimbres <rubenszmm@gmail.com>
2020-12-14 11:17:46 -0300

    Create dynamic_fibo_goals.pas

---
+++ Pascal/dynamic_fibo_goals.pas
@@ -0,0 +1,27 @@
+var
+  maior : Float;
+  menor : Float;
+  diff : Float;
+  lower:Float;
+  higher : Float;
+  alvo : Float;
+  alvo2: Float;
+begin
+  maior := Highest(Close,144)[22];
+  menor := Lowest(Close,144);
+  diff:=maior-menor;
+  lower:= (menor+0.382*diff);
+  higher:= (menor+0.618*diff);
+  alvo:=maior*1.11;
+  alvo2:=maior*1.328;
+  
+  SetPlotColor(1,clNavy);
+  SetPlotColor(2,clNavy);
+  
+  SetPlotWidth(1,2);
+  SetPlotWidth(2,2);
+  
+  Plot(alvo);
+  Plot2(alvo2);
+
+end;
