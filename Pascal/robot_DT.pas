17ee558000ed9264ceafde55f47988fed1f46042
Rubens Zimbres <rubenszmm@gmail.com>
2021-01-05 22:15:17 -0300

    Create robot_DT.pas

---
+++ Pascal/robot_DT.pas
@@ -0,0 +1,13 @@
+begin
+
+  if (Media(13,Close)<Media(3,Close)) and (MACD(35,26,9)>MACD(35,26,9)[1])  Then 
+  begin 
+    PaintBar(clLime);// ALTA
+  end
+  else if (Media(13,Close)>Media(3,Close)) and (MACD(35,26,9)<MACD(35,26,9)[1])  Then 
+  begin
+    PaintBar(clRed);// ALTA
+  end
+  else PaintBar(clBlue);  
+
+end;
