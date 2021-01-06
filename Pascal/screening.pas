e97bda65efdec129ce188af8bae7e8056cf0c5f9
Rubens Zimbres <rubenszmm@gmail.com>
2021-01-06 14:01:33 -0300

    Update screening.pas

--- Pascal/screening.pas
+++ Pascal/screening.pas
@@ -1,12 +1,12 @@
 begin
 
-  if (Media(13,Close)<Media(3,Close)) and (MACD(35,26,9)>MACD(35,26,9)[1])  Then 
+  if (Media(13,Close)[1]<Media(3,Close)[1]) and (MACD(35,26,9)[1]>MACD(35,26,9)[2])  Then 
   begin 
     PaintBar(clLime);// ALTA
   end
-  else if (Media(13,Close)>Media(3,Close)) and (MACD(35,26,9)<MACD(35,26,9)[1])  Then 
+  else if (Media(13,Close)[1]>Media(3,Close)[1]) and (MACD(35,26,9)[1]<MACD(35,26,9)[2])  Then 
   begin
     PaintBar(clRed);// ALTA
   end
-  
-  end;
+  else Select
+end;
