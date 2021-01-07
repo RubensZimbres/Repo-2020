841b28f2a1f5ba304f126538e8adfb6485e20d6d
Rubens Zimbres <rubenszmm@gmail.com>
2021-01-07 18:38:18 -0300

    Update screening.pas

--- Pascal/screening.pas
+++ Pascal/screening.pas
@@ -1,12 +1,39 @@
+var
+  ave0: Real;
+  diff: Real;
+  hist:Real;
+  ave2: Real;
+  
 begin
+  
+  hist:=MACD(21,15,9)|1|;
+  ave0:=MediaExp(3,Close);
+  ave2:=MediaExp(13,Close);
+  diff:=ave0-ave2;
 
-  if (Media(13,Close)[1]<Media(3,Close)[1]) and (MACD(35,26,9)[1]>MACD(35,26,9)[2])  Then 
-  begin 
-    PaintBar(clLime);// ALTA
-  end
-  else if (Media(13,Close)[1]>Media(3,Close)[1]) and (MACD(35,26,9)[1]<MACD(35,26,9)[2])  Then 
-  begin
-    PaintBar(clRed);// ALTA
-  end
-  else Select
+
+  se (hist<0) e (hist>-0.02) e (diff<0) e (MACD(21,15,9)[1]|1|>=0) e (ADX(9,9)>ADX(9,9)[1]) então Select;
+
+        
+end;
+
+____________________________
+
+var
+  ave0: Real;
+  diff: Real;
+  hist:Real;
+  ave2: Real;
+  
+begin
+  
+  hist:=MACD(21,15,9)|1|;
+  ave0:=MediaExp(3,Close);
+  ave2:=MediaExp(13,Close);
+  diff:=ave0-ave2;
+
+
+  se (hist>0) e (hist<0.02) e (diff>0) e (MACD(21,15,9)[1]|1|<=0) e (ADX(9,9)>ADX(9,9)[1]) então Select;
+
+        
 end;
